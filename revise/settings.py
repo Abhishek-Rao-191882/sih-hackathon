f7718010616b06ad43ba8c6062ec37292aef87c8
Abhishek Rao <arao8732@gmail.com>
2022-03-13 00:44:25 +0530

    update
    Signed-off-by: Abhishek Rao <arao8732@gmail.com>

--- revise/settings.py
+++ revise/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,4 +134,3 @@ REST_FRAMEWORK = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-django_heroku.settings(locals())
